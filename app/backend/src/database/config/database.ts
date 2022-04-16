32f1496c1652b3e808ee47a7a8fbbd73968df526
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-04-16 15:13:54 -0300

    req 01 02 03

--- app/backend/src/database/config/database.ts
+++ app/backend/src/database/config/database.ts
@@ -3,7 +3,7 @@ import { Options } from 'sequelize';
 
 const config: Options = {
   username: process.env.DB_USER || 'root',
-  password: process.env.DB_PASS || 'Kd415263!',
+  password: process.env.DB_PASS || '123456',
   database: process.env.DB_NAME || 'TRYBE_FUTEBOL_CLUBE',
   host: process.env.DB_HOST || '127.0.0.1',
   port: Number(process.env.DB_PORT) || 3002,
